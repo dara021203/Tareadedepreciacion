a478e91cbcf7bee1908700fe99e4338e03ab2536
dara021203 <Dara021203@gmail.com>
2022-03-21 18:52:22 -0600

    Eliminar

---
+++ AppCore/Services/BaseServices.cs
@@ -0,0 +1,36 @@
+﻿using AppCore.IServices;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Services
+{
+    public abstract class BaseServices<T>: IServices<T>
+    {
+        private IModel<T> Model;
+        protected BaseServices(IModel<T> model)
+        {
+            this.Model = model;
+        }
+
+        public void Add(T t)
+        {
+             Model.Add(t);
+        }
+
+        public bool Delete(T t)
+        {
+            return Model.Delete(t);
+        }
+
+        public List<T> Read()
+        {
+            return Model.Read();
+        }
+
+      
+    }
+}
---
+++ Infraestructure/Repository/BinaryActivoRepository.cs
@@ -0,0 +1,72 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository
+{
+    public class BinaryActivoRepository : IActivoModel
+    {
+        private RAFContext<Activo> context;
+        private const int SIZE = 119;
+        public BinaryActivoRepository()
+        {
+            context = new RAFContext<Activo>("activo", SIZE);
+        }
+        public void Add(Activo t)
+        {
+            try
+            {
+                context.Create<Activo>(t);
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+        }
+
+        public Activo GetById(int id)
+        {
+            try
+            {
+                return context.Get<Activo>(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Activo> Read()
+        {
+            try
+            {
+                return context.GetAll<Activo>();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool Delete(Activo t)
+        {
+            try
+            {
+                return context.Delete<Activo>(t);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+       
+        }
+    }
+
